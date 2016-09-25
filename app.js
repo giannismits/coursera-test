d675e009ae24c88b117a8510ad00430e0e62a046
giannismits <giannis.mits9@gmail.com>
2016-09-25 17:39:58 +0300

    module2-assigment

--- app.js
+++ app.js
@@ -1,31 +1,100 @@
 (function(){
-  'use strict';
+'use strict';
 
-  angular.module('lunchApp',[])
-  .controller('lunchController',LunchController);
-LunchController.$inject['$scope'];
-  function LunchController($scope){
-    $scope.order=null;
-      $scope.check=function(){
-        var theorder=$scope.order;
-        if($scope.order==null){
-          $scope.message="Enter data first";
-        }else{
-          chackingtheorder();
-        }
-      function chackingtheorder(){
-        var arrayofStrings=theorder.split(",");
-        console.log(arrayofStrings);
-        var amount=arrayofStrings.length;
-        if(amount<=3){
-          $scope.message="Enjoy";
+angular.module('shoppingListApp',[])
+.controller('shoppingListFull',shoppingListFull)
+.controller('shoppingListEmpty',shoppingListEmpty)
+.provider('shoppingService',serviceProvider);
 
-        }else {
-          $scope.message="too much";
-        }
-      };
-      }
+//first controller of full list item
+shoppingListFull.$inject=['shoppingService'];
+function shoppingListFull(shoppingService){
+  var list1=this;
 
+  list1.items=shoppingService.showList();
+
+  list1.removeItem=function(index){
+    list1.check=function(){
+      return true;
+    };
+    try{
+      shoppingService.remove(index);
+    }catch(error){
+      list1.errorMessage=error.message;
+    }
+  };
+
+}
+
+//second controller of empty (initially) list.
+shoppingListEmpty.$inject=['shoppingService'];
+function shoppingListEmpty(shoppingService){
+  var list2=this;
+
+
+list2.items=shoppingService.showTheBasketFull();
+
+
+}
+
+//service
+function shoppingService(){
+  var service=this;
+var itemsInList=[
+  {
+    name:"cookies",
+    quantity:"10"
+  },
+  {
+    name:"pizza",
+    quantity:"10"
+  },
+  {
+    name:"chips",
+    quantity:"10"
+  },
+  {
+    name:"cake",
+    quantity:"10"
+  },
+  {
+    name:"soda",
+    quantity:"10"
   }
+];
+service.showList=function(){
+  return itemsInList;
+};
+
+var emptyList=[];
+
+
+service.remove=function(index){
+  var item=itemsInList[index];
+  var newItem={
+    name:item.name,
+    quantity:item.quantity
+  };
+  emptyList.push(newItem);
+  itemsInList.splice(index,1);
+if(itemsInList.length==0){
+  throw new Error("Everything is bought!");
+}
+};
+
+
+service.showTheBasketFull=function(){
+  return emptyList;
+};
+}
+//provider
+function serviceProvider(){
+  var provider=this;
+
+  provider.$get=function(){
+    var shopping=new shoppingService();
+    return shopping;
+  };
+}
 
 })();
